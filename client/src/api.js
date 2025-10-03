2495b25dc0f6c2b618ba7cda315b8479eeca6df5
Priyansh Keer <153297539+priyansh-k18@users.noreply.github.com>
2025-10-04 02:12:51 +0530

    ko

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     // Change process.env.baseURL to import.meta.env.VITE_baseURL
-    baseURL: import.meta.env.VITE_baseURL,
+    baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export default API;
